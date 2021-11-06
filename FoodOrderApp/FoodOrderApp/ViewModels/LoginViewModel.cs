e54d4379538affde229934f416c098925f68b11f
Phuc Tran <tr.ph3322@gmail.com>
2021-11-06 11:57:54 +0700

    update LoginWindow

--- FoodOrderApp/FoodOrderApp/ViewModels/LoginViewModel.cs
+++ FoodOrderApp/FoodOrderApp/ViewModels/LoginViewModel.cs
@@ -4,10 +4,12 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
+using System.Windows.Controls;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Animation;
 using FoodOrderApp.Views;
+using FoodOrderApp.Views.UserControls;
 
 namespace FoodOrderApp.ViewModels
 {
@@ -15,11 +17,31 @@ namespace FoodOrderApp.ViewModels
     {
         public ICommand OpenForgotPasswordWDCommand { get; set; }
         public ICommand OpenSignUpWDCommand { get; set; }
-
+        public ICommand LoadedCommand { get; set; }
+        public ICommand CloseWindowCommand { get; set; }
         public LoginViewModel()
         {
             OpenForgotPasswordWDCommand = new RelayCommand<LoginWindow>((parameter) => true, (parameter) => OpenForgotPasswordWindow(parameter));
             OpenSignUpWDCommand = new RelayCommand<LoginWindow>((parameter) => true, (parameter) => OpenSignUpWindow(parameter));
+            LoadedCommand = new RelayCommand<ControlBarUC>(p => true, (p) => Loaded(p));
+            CloseWindowCommand = CloseWindowCommand = new RelayCommand<UserControl>((p) => p == null ? false : true, p => {
+                if(CustomMessageBox.Show("Thoát ứng dụng?", MessageBoxButton.OKCancel) == MessageBoxResult.OK)
+                {
+                    FrameworkElement window = ControlBarViewModel.GetParentWindow(p);
+                    var w = window as Window;
+
+                    if (w != null)
+                    {
+                        w.Close();
+                    }
+                }
+            });
+        }
+
+        public void Loaded(ControlBarUC cb)
+        {
+            cb.closeBtn.Command = CloseWindowCommand;
+            cb.closeBtn.CommandParameter = cb;
         }
 
         public void OpenForgotPasswordWindow(LoginWindow parameter)
