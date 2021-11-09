39635c0dd5f68ecf841197c05b681c75c8ecb046
Phuc Tran <tr.ph3322@gmail.com>
2021-11-09 22:59:12 +0700

    add code logic for MainWindow

--- FoodOrderApp/FoodOrderApp/ViewModels/MainViewModel.cs
+++ FoodOrderApp/FoodOrderApp/ViewModels/MainViewModel.cs
@@ -1,4 +1,5 @@
-﻿using FoodOrderApp.Views.UserControls;
+﻿using FoodOrderApp.Views;
+using FoodOrderApp.Views.UserControls;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -14,10 +15,11 @@ namespace FoodOrderApp.ViewModels
     {
         public ICommand LoadedCommand { get; set; }
         public ICommand CloseWindowCommand { get; set; }
+        public ICommand SwitchTabCommand { get; set; }
 
         public MainViewModel()
         {
-            LoadedCommand = new RelayCommand<ControlBarUC>(parameter => true, parameter => Loaded(parameter));
+            LoadedCommand = new RelayCommand<MainWindow>(parameter => true, parameter => Loaded(parameter));
             CloseWindowCommand = CloseWindowCommand = new RelayCommand<UserControl>((p) => p == null ? false : true, p => {
                 if (CustomMessageBox.Show("Thoát ứng dụng?", MessageBoxButton.OKCancel, MessageBoxImage.Question) == MessageBoxResult.OK)
                 {
@@ -30,12 +32,38 @@ namespace FoodOrderApp.ViewModels
                     }
                 }
             });
+            SwitchTabCommand = new RelayCommand<MainWindow>(p => true, (p) => SwitchTab(p));
         }
 
-        private void Loaded(ControlBarUC cb)
+        private void Loaded(MainWindow mainWindow)
         {
-            cb.closeBtn.Command = CloseWindowCommand;
-            cb.closeBtn.CommandParameter = cb;
+            mainWindow.ucWindow.Children.Add(new MenuUC());
+            
+            mainWindow.controlBar.closeBtn.Command = CloseWindowCommand;
+            mainWindow.controlBar.closeBtn.CommandParameter = mainWindow.controlBar;
+        }
+
+        private void SwitchTab(MainWindow mainWindow) 
+        {
+            switch (mainWindow.listViewMenu.SelectedIndex)
+            {
+                case 0:
+                    mainWindow.ucWindow.Children.Clear();
+                    mainWindow.ucWindow.Children.Add(new MenuUC());
+                    break;
+                case 1:
+                    mainWindow.ucWindow.Children.Clear();
+                    mainWindow.ucWindow.Children.Add(new CartUC());
+                    break;
+                case 2:
+                    mainWindow.ucWindow.Children.Clear();
+                    mainWindow.ucWindow.Children.Add(new AccountUC());
+                    break;
+                case 3:
+                    mainWindow.ucWindow.Children.Clear();
+                    mainWindow.ucWindow.Children.Add(new ContactUC());
+                    break;
+            }
         }
     }
 }
