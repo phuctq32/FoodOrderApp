4197ede90616ba08d6968a0e47fba02b04a4db67
Đức Nguyễn Trí <20520453@ms.uit.edu.vn>
2021-11-09 20:02:11 +0700

    add helper double to text decoration converter

---
+++ FoodOrderApp/FoodOrderApp/Helpers/DoubleToTextDecorationsConverter.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+
+namespace FoodOrderApp.Helpers
+{
+    internal class DoubleToTextDecorationConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter,
+                              CultureInfo culture)
+        {
+            if (value != null)
+            {
+                // kiểm tra xem value = discount > 0 thì gạch ngang giá cũ
+                return (Double.Parse(value.ToString().Replace("%", " ")) > 0) ? TextDecorations.Strikethrough : value;
+            }
+            return value;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter,
+                              CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
