929bf5221b9b3ebaecc7d1bdd2ab75375999ab98
Phuc Tran <tr.ph3322@gmail.com>
2021-11-11 14:04:31 +0700

    update menu logic code

--- FoodOrderApp/FoodOrderApp/ViewModels/MenuViewModel.cs
+++ FoodOrderApp/FoodOrderApp/ViewModels/MenuViewModel.cs
@@ -1,12 +1,29 @@
-﻿using System;
+﻿using FoodOrderApp.Models;
+using FoodOrderApp.Views.UserControls;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Input;
 
 namespace FoodOrderApp.ViewModels
 {
     internal class MenuViewModel : BaseViewModel
     {
+        public ICommand LoadedCommand { get; set; }
+        public List<PRODUCT> Products { get; set; }
+
+        public MenuViewModel()
+        {
+            
+            LoadedCommand = new RelayCommand<MenuUC>(p => p == null ? false : true, p => Loaded(p));
+        }
+
+        private void Loaded(MenuUC menu)
+        {
+            Products = Data.Ins.DB.PRODUCTs.ToList();
+            menu.ViewListProducts.ItemsSource = Products;
+        }
     }
 }
