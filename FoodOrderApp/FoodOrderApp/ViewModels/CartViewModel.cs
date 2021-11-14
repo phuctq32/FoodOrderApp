abb259d66fddbf609a55168f71f7ef8e29712905
Đức Nguyễn Trí <20520453@ms.uit.edu.vn>
2021-11-14 16:01:02 +0700

    add My order UC UI

--- FoodOrderApp/FoodOrderApp/ViewModels/CartViewModel.cs
+++ FoodOrderApp/FoodOrderApp/ViewModels/CartViewModel.cs
@@ -14,6 +14,7 @@ namespace FoodOrderApp.ViewModels
     public class CartViewModel : BaseViewModel
     {
         public ICommand LoadedCommand { get; set; }
+
         public CartViewModel()
         {
             LoadedCommand = new RelayCommand<CartUC>(p => true, p => DisplayCart(p));
@@ -21,13 +22,13 @@ namespace FoodOrderApp.ViewModels
 
         private void DisplayCart(CartUC parameter)
         {
-            List<CART> currentUserCarts = Data.Ins.DB.CARTs.Where(cart => cart.USERNAME_ == CurrentAccount.Username).ToList();
-            foreach (var item in currentUserCarts)
-            {
-                var product = Data.Ins.DB.PRODUCTs.Where(p => p.ID_ == item.PRODUCT_) as PRODUCT;
-                CurrentAccount.ProductsInCart.Add(product);
-            }
-            parameter.cartList.ItemsSource = CurrentAccount.ProductsInCart;
+            //List<CART> currentUserCarts = Data.Ins.DB.CARTs.Where(cart => cart.USERNAME_ == CurrentAccount.Username).ToList();
+            //foreach (var item in currentUserCarts)
+            //{
+            //    var product = Data.Ins.DB.PRODUCTs.Where(p => p.ID_ == item.PRODUCT_) as PRODUCT;
+            //    CurrentAccount.ProductsInCart.Add(product);
+            //}
+            //parameter.cartList.ItemsSource = CurrentAccount.ProductsInCart;
         }
     }
 }
