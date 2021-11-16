d4f1884b30f2b33301f2ea53cb56ef46c2791487
Đam Sa <20521154@ms.uit.edu.vn>
2021-11-16 21:02:32 +0700

    Add Item to CARTS

--- FoodOrderApp/FoodOrderApp/ViewModels/MenuViewModel.cs
+++ FoodOrderApp/FoodOrderApp/ViewModels/MenuViewModel.cs
@@ -37,7 +37,24 @@ namespace FoodOrderApp.ViewModels
 
         private void AddToCart(ListViewItem parameter)
         {
-           
+            try
+            {
+                var item = parameter.DataContext as PRODUCT;
+                int cartsCount = Data.Ins.DB.CARTs.Where(x => x.USERNAME_ == CurrentAccount.Username && x.PRODUCT_ == item.ID_).Count();
+                int idCarts = Data.Ins.DB.CARTs.Count();
+                if (cartsCount == 0)
+                {
+                    Data.Ins.DB.CARTs.Add(new CART() { ID_ = item.ID_, PRODUCT_ = item.ID_, USERNAME_ = CurrentAccount.Username, AMOUNT_ = 1 });
+                    Data.Ins.DB.SaveChanges();
+                    CustomMessageBox.Show("Đã thêm " + item.NAME_ + " vào giỏ hàng thành công", MessageBoxButton.OK);
+                }
+                else
+                    CustomMessageBox.Show("Món ăn " + item.NAME_ + " đã có sẵn trong giỏ hàng", MessageBoxButton.OK);
+            }
+            catch
+            {
+                CustomMessageBox.Show("Lỗi cơ sở dữ liệu");
+            }
         }
     }
 }
