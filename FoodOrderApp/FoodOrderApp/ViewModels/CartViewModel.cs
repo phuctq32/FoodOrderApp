d620ef4df31aa3e1bd14c20bac8f69c4b976586c
Đam Sa <20521154@ms.uit.edu.vn>
2021-11-17 16:24:52 +0700

    DeleteCart not propertychange

--- FoodOrderApp/FoodOrderApp/ViewModels/CartViewModel.cs
+++ FoodOrderApp/FoodOrderApp/ViewModels/CartViewModel.cs
@@ -14,10 +14,12 @@ namespace FoodOrderApp.ViewModels
     public class CartViewModel : BaseViewModel
     {
         public ICommand LoadedCommand { get; set; }
+        public ICommand DeleteCartCommand { get; set; }
 
         public CartViewModel()
         {
             LoadedCommand = new RelayCommand<CartUC>(p => true, p => DisplayCart(p));
+            DeleteCartCommand = new RelayCommand<ListViewItem>((parameter) => { return true; }, (parameter) => DeleteCart(parameter));
         }
 
         private void DisplayCart(CartUC parameter)
@@ -30,6 +32,23 @@ namespace FoodOrderApp.ViewModels
                 loadedListCart.Add(product);
             }
             parameter.cartList.ItemsSource = loadedListCart;
+        }
+        private void DeleteCart(ListViewItem parameter)
+        {
+            try
+            {
+
+                PRODUCT item = parameter.DataContext as PRODUCT;
+                CART del = Data.Ins.DB.CARTs.Where(x => x.PRODUCT_ == item.ID_ && x.USERNAME_ == CurrentAccount.Username).SingleOrDefault();
+                Data.Ins.DB.CARTs.Remove(del);
+                Data.Ins.DB.SaveChanges();
+                CustomMessageBox.Show("Xóa thành công");
+
+            }
+            catch
+            {
+                CustomMessageBox.Show("Lỗi xóa cart");
+            }
         }
     }
 }
