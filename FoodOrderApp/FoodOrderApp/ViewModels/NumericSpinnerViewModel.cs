ba1c3a90dc6ea24d34ccb4e83e69fd60c811952e
Phuc Tran <tr.ph3322@gmail.com>
2021-11-17 17:02:27 +0700

    completed logic code for NumericSpinner

---
+++ FoodOrderApp/FoodOrderApp/ViewModels/NumericSpinnerViewModel.cs
@@ -0,0 +1,73 @@
+﻿using FoodOrderApp.Models;
+using FoodOrderApp.Views.UserControls;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
+using System.Windows.Media;
+
+namespace FoodOrderApp.ViewModels
+{
+    class NumericSpinnerViewModel
+    {
+        public ICommand DownCommand { get; set; }
+        public ICommand UpCommand { get; set; }
+        public NumericSpinnerViewModel()
+        {
+            DownCommand = new RelayCommand<NumericSpinner>(p => true, p => Down(p));
+            UpCommand = new RelayCommand<NumericSpinner>(p => true, p => Up(p));
+        }
+        private void Down(NumericSpinner parameter)
+        {
+            int amount = Int32.Parse(parameter.tb_main.Text.ToString());
+            if (amount == 1)
+            {
+                if (CustomMessageBox.Show("Xóa món ăn khỏi giỏ hàng?", MessageBoxButton.OKCancel, MessageBoxImage.Question) == MessageBoxResult.OK)
+                {
+                    try
+                    {
+                        var lvi = GetAncestorOfType<ListViewItem>(parameter);
+                        PRODUCT product = lvi.DataContext as PRODUCT;
+                        CART cartToDelete = Data.Ins.DB.CARTs.Where(x => x.PRODUCT_ == product.ID_ && x.USERNAME_ == CurrentAccount.Username).SingleOrDefault();
+                        Data.Ins.DB.CARTs.Remove(cartToDelete);
+                        Data.Ins.DB.SaveChanges();
+                        CustomMessageBox.Show("Xóa thành công!", MessageBoxButton.OK, MessageBoxImage.None);
+                    }
+                    catch
+                    {
+                        CustomMessageBox.Show("Lỗi cơ sở dữ liệu!", MessageBoxButton.OK, MessageBoxImage.Error);
+                    }
+                }
+            }
+            else
+            {
+                amount--;
+                parameter.tb_main.Text = amount.ToString();
+            }
+        }
+        private void Up(NumericSpinner parameter)
+        {
+            int amount = Int32.Parse(parameter.tb_main.Text.ToString());
+            if (amount == int.MaxValue)
+            {
+                return;
+            }
+            else
+            {
+                amount++;
+                parameter.tb_main.Text = amount.ToString();
+            }
+        }
+        public T GetAncestorOfType<T>(FrameworkElement child) where T : FrameworkElement
+        {
+            var parent = VisualTreeHelper.GetParent(child);
+            if (parent != null && !(parent is T))
+                return (T)GetAncestorOfType<T>((FrameworkElement)parent);
+            return (T)parent;
+        }
+    }
+}
--- FoodOrderApp/FoodOrderApp/Views/UserControls/NumericSpinner.xaml.cs
+++ FoodOrderApp/FoodOrderApp/Views/UserControls/NumericSpinner.xaml.cs
@@ -22,43 +22,9 @@ namespace FoodOrderApp.Views.UserControls
     /// </summary>
     public partial class NumericSpinner : UserControl
     {
-        private decimal value;
         public NumericSpinner()
         {
             InitializeComponent();
-            Value = 1;
-        }
-
-        public decimal Value { get => value; set {
-                if(value > decimal.MaxValue)
-                {
-                    this.value = decimal.MaxValue;
-                }
-                else if(value < decimal.MinValue)
-                {
-                    this.value = decimal.MinValue;
-                }
-                else
-                {
-                    this.value = value;
-                }
-            }
-        }
-
-        private void cmdDown_Click(object sender, RoutedEventArgs e)
-        {
-            if (Value == 1)
-            {
-                CustomMessageBox.Show("Bạn có muốn xóa sản phảm khỏi giỏ hàng không?", MessageBoxButton.OKCancel, MessageBoxImage.Question);
-            }
-            Value -= 1;
-            tb_main.Text = Value.ToString();
-        }
-
-        private void cmdUp_Click(object sender, RoutedEventArgs e)
-        {
-            Value += 1;
-            tb_main.Text = Value.ToString();
         }
     }
 }
