e09fe1a188537ce9edf1425611844c9036633091
Kim Dien <85953327+Kimdien2206@users.noreply.github.com>
2021-11-20 17:23:05 +0700

    Account

--- FoodOrderApp/FoodOrderApp/ViewModels/AccountViewModel.cs
+++ FoodOrderApp/FoodOrderApp/ViewModels/AccountViewModel.cs
@@ -1,12 +1,145 @@
-﻿using System;
+﻿using FoodOrderApp;
+using FoodOrderApp.Models;
+using FoodOrderApp.Views;
+using FoodOrderApp.Views.UserControls;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
+using System.IO;
+using Azure.Storage.Blobs;
+using Azure.Storage.Blobs.Models;
+using System.Windows.Forms;
+using Microsoft.Win32;
+using System.Media;
+using System.Drawing;
+using System.Windows.Media.Imaging;
 
 namespace FoodOrderApp.ViewModels
 {
-    class AccountViewModel
+    internal class AccountViewModel : BaseViewModel
     {
+        public ICommand UploadImageCommand { get; set; }
+        public ICommand ChangeInfoCommand { get; set; }
+        public ICommand LoadedCommand { get; set; }
+        private USER user;
+        private string FULLNAME;
+
+        public string FULLNAME_
+        { get => FULLNAME; set { FULLNAME = value; OnPropertyChanged("FULLNAME_"); } }
+
+        private string AVATAR;
+
+        public string AVATAR_
+        { get => AVATAR; set { AVATAR = value; OnPropertyChanged("AVATAR_"); } }
+
+        private string mail;
+
+        public string Mail
+        { get => mail; set { mail = value; OnPropertyChanged("Mail"); } }
+
+        private string phone;
+
+        public string Phone
+        { get => phone; set { phone = value; OnPropertyChanged("Phone"); } }
+
+        private string userName;
+
+        public string UserName
+        { get => userName; set { userName = value; OnPropertyChanged("UserName"); } }
+
+        private string address;
+
+        public string Address
+        { get => address; set { address = value; OnPropertyChanged("Address"); } }
+
+        public AccountViewModel()
+        {
+            UploadImageCommand = new RelayCommand<AccountUC>((parameter) => true, (parameter) => UploadImage(parameter));
+            ChangeInfoCommand = new RelayCommand<AccountUC>((parameter) => true, (paramater) => ChangeInfo(paramater));
+            LoadedCommand = new RelayCommand<AccountUC>((parameter) => true, (paramater) => loaded(paramater));
+        }
+
+        private void loaded(AccountUC paramater)
+        {
+            user = Data.Ins.DB.USERS.Where(x => x.USERNAME_ == CurrentAccount.Username).SingleOrDefault();
+            AVATAR_ = user.AVATAR_;
+            FULLNAME_ = user.FULLNAME_;
+            Phone = user.PHONE_;
+            UserName = user.USERNAME_;
+            Mail = user.EMAIL_;
+            Address = user.ADDRESS_;
+        }
+
+        public void UploadImage(AccountUC accountUC)
+        {
+            System.Windows.Forms.OpenFileDialog openFileDialog = new System.Windows.Forms.OpenFileDialog();
+            openFileDialog.Filter = "Image files | *.jpg; *.png | All files | *.*";
+
+            //Create connection to Storage
+
+            string containerName = "avatar";
+            string connectionString = "DefaultEndpointsProtocol=https;AccountName=phong;AccountKey=/4FmL2uepULrqhajPWW1odbS70e5L/SEYVyO7ej3Zyzgh5cw61MysAf+f73I3euXcATYPUi8nJHQ0la8XB7Ccg==;EndpointSuffix=core.windows.net";
+            BlobContainerClient containerClient = new BlobContainerClient(connectionString, containerName);
+
+            //Update Image
+
+            if (openFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                //Get name of Image
+
+                string[] filename = Path.GetFileName(openFileDialog.FileName).Split('.');
+
+                //Delete old Image
+
+                BlobClient blobClient = new BlobClient(connectionString, containerName, CurrentAccount.Username + "." + AVATAR_.Split('.')[5]);
+                blobClient.Delete();
+
+                //Start upload
+
+                using (MemoryStream stream = new MemoryStream(File.ReadAllBytes(openFileDialog.FileName)))
+                {
+                    //Upload new Image
+
+                    containerClient.UploadBlob(CurrentAccount.Username + "." + filename[1], stream);
+                    CustomMessageBox.Show("Thay đổi ảnh thành công", MessageBoxButton.OKCancel, MessageBoxImage.Information);
+                }
+
+                //Update new Image link
+
+                USER user = Data.Ins.DB.USERS.Where(x => x.USERNAME_ == CurrentAccount.Username).SingleOrDefault();
+                user.AVATAR_ = "https://phong.blob.core.windows.net/avatar/" + CurrentAccount.Username + "." + filename[1];
+
+                //Save database change
+
+                Data.Ins.DB.SaveChanges();
+
+                //Load new image
+
+                System.Windows.Media.Imaging.BitmapImage bitmap = new BitmapImage();
+                bitmap.BeginInit();
+                bitmap.UriSource = new Uri(openFileDialog.FileName, UriKind.Absolute);
+                bitmap.EndInit();
+                accountUC.ImgBrush.ImageSource = bitmap;
+            }
+        }
+
+        public void ChangeInfo(AccountUC accountUC)
+        {
+            ChangeInformationWindow changeInformationWindow = new ChangeInformationWindow();
+            changeInformationWindow.ShowDialog();
+
+            USER user = Data.Ins.DB.USERS.Where(x => x.USERNAME_ == CurrentAccount.Username).SingleOrDefault();
+            AVATAR_ = user.AVATAR_;
+            FULLNAME_ = user.FULLNAME_;
+            Phone = user.PHONE_;
+            UserName = user.USERNAME_;
+            Mail = user.EMAIL_;
+            Address = user.ADDRESS_;
+        }
     }
 }
---
+++ FoodOrderApp/FoodOrderApp/ViewModels/ChangeInformationViewModel.cs
@@ -0,0 +1,79 @@
+﻿using FoodOrderApp.Models;
+using FoodOrderApp.Views;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Mail;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
+
+namespace FoodOrderApp.ViewModels
+{
+    internal class ChangeInformationViewModel : BaseViewModel
+    {
+        public ICommand SaveInfoCommand { get; set; }
+
+        //public ICommand PasswordChangedCommand { get; set; }
+        //public ICommand RePasswordChangedCommand { get; set; }
+        //public ICommand ActivationCommand { get; set; }
+
+        private USER user;
+
+        private string mail;
+
+        public string Mail
+        { get => mail; set { mail = value; OnPropertyChanged("Mail"); } }
+
+        private string fullname;
+
+        public string FULLNAME_
+        { get => fullname; set { fullname = value; OnPropertyChanged("Fullname"); } }
+
+        private string phone;
+
+        public string Phone
+        { get => phone; set { phone = value; OnPropertyChanged("Phone"); } }
+
+        private string address;
+
+        public string Address
+        { get => address; set { address = value; OnPropertyChanged("Address"); } }
+
+        public ChangeInformationViewModel()
+        {
+            SaveInfoCommand = new RelayCommand<ChangeInformationWindow>((parameter) => true, (parameter) => SaveChange(parameter));
+            user = Data.Ins.DB.USERS.Where(x => x.USERNAME_ == CurrentAccount.Username).SingleOrDefault();
+            this.Phone = user.PHONE_;
+            this.FULLNAME_ = user.FULLNAME_;
+            this.Address = user.ADDRESS_;
+            this.Mail = user.EMAIL_;
+        }
+
+        public void SaveChange(ChangeInformationWindow changeInformationWD)
+        {
+            user.PHONE_ = changeInformationWD.txtPhone.Text;
+            user.FULLNAME_ = changeInformationWD.txtFullname.Text;
+            user.ADDRESS_ = changeInformationWD.txtAddress.Text;
+            try
+            {
+                Data.Ins.DB.SaveChanges();
+                FULLNAME_ = user.FULLNAME_;
+                Phone = user.PHONE_;
+                Mail = user.EMAIL_;
+                Address = user.ADDRESS_;
+                CustomMessageBox.Show("Thay đổi thành công", MessageBoxButton.OK, MessageBoxImage.Information);
+            }
+            catch
+            {
+                CustomMessageBox.Show("Thay đổi không thành công", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+            changeInformationWD.Close();
+        }
+    }
+}
+
