40b82c4562131bce3a85077591b243969474a9ce
Kim Dien <85953327+Kimdien2206@users.noreply.github.com>
2021-11-20 16:16:30 +0700

    Edit Product 1

---
+++ FoodOrderApp/FoodOrderApp/ViewModels/EditProductViewModel.cs
@@ -0,0 +1,30 @@
+﻿using FoodOrderApp.Models;
+using FoodOrderApp.Views.UserControls.Admin;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
+
+namespace FoodOrderApp.ViewModels
+{
+    internal class EditProductViewModel : BaseViewModel
+    {
+        public ICommand LoadedCommand { get; set; }
+
+        public List<PRODUCT> pRODUCTs;
+        public EditProductViewModel()
+        {
+            LoadedCommand = new RelayCommand<EditProductUC>((parameter) => true, (parameter) => Loaded(parameter));
+        }
+        public void Loaded(EditProductUC editProductUC)
+        {
+            pRODUCTs = Data.Ins.DB.PRODUCTs.ToList();
+            editProductUC.
+        }
+    }
+}
