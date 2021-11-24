8e49bc304f9ba6fe5aa9c841050a08001f772185
Phuc Tran <tr.ph3322@gmail.com>
2021-11-24 10:53:46 +0700

    completed Order Detail UI - added rating bar function

--- FoodOrderApp/FoodOrderApp/ViewModels/MyOrderViewModel.cs
+++ FoodOrderApp/FoodOrderApp/ViewModels/MyOrderViewModel.cs
@@ -1,12 +1,26 @@
-﻿using System;
+﻿using FoodOrderApp.Views;
+using MaterialDesignThemes.Wpf;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Controls;
+using System.Windows.Input;
 
 namespace FoodOrderApp.ViewModels
 {
     internal class MyOrderViewModel : BaseViewModel
     {
+        public ICommand RatingCommand { get; set; }
+        public MyOrderViewModel()
+        {
+            RatingCommand = new RelayCommand<RatingBar>(p => true, p => RatingChanged(p));
+        }
+        private void RatingChanged(RatingBar p)
+        {
+            //CustomMessageBox.Show(p.Value.ToString());
+            //p.IsEnabled = false;
+        }
     }
 }
