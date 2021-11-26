e667d2fc7194113817813993948e64eaab803489
phongbj <20520364@gm.uit.edu.vn>
2021-11-26 23:18:11 +0700

    AddToCart command

--- FoodOrderApp/FoodOrderApp/ViewModels/MenuViewModel.cs
+++ FoodOrderApp/FoodOrderApp/ViewModels/MenuViewModel.cs
@@ -152,25 +152,25 @@ namespace FoodOrderApp.ViewModels
         //}
         private void AddToCart(ListViewItem parameter)
         {
-            //try
-            //{
-            //    var item = parameter.DataContext as PRODUCT;
-            //    int cartsCount = Data.Ins.DB.CARTs.Where(x => x.USERNAME_ == CurrentAccount.Username && x.PRODUCT_ == item.ID_).Count();
-            //    int idCarts = Data.Ins.DB.CARTs.Count();
-            //    if (cartsCount == 0)
-            //    {
-            //        string tmpID = CurrentAccount.Username +"_"+ item.ID_;
-            //        Data.Ins.DB.CARTs.Add(new CART() { ID_ = tmpID, PRODUCT_ = item.ID_, USERNAME_ = CurrentAccount.Username, AMOUNT_ = 1 });
-            //        Data.Ins.DB.SaveChanges();
-            //        CustomMessageBox.Show("Đã thêm " + item.NAME_.ToString() + " vào giỏ hàng thành công", MessageBoxButton.OK, MessageBoxImage.Asterisk);
-            //    }
-            //    else
-            //        CustomMessageBox.Show("Món ăn " + item.NAME_.ToString() + " đã có sẵn trong giỏ hàng", MessageBoxButton.OK, MessageBoxImage.Asterisk);
-            //}
-            //catch
-            //{
-            //    CustomMessageBox.Show("Lỗi cơ sở dữ liệu", MessageBoxButton.OK, MessageBoxImage.Error);
-            //}
+            try
+            {
+                var item = parameter.DataContext as PRODUCT;
+                int cartsCount = Data.Ins.DB.CARTs.Where(x => x.USERNAME_ == CurrentAccount.Username && x.PRODUCT_ == item.ID_).Count();
+                int idCarts = Data.Ins.DB.CARTs.Count();
+                if (cartsCount == 0)
+                {
+                    string tmpID = CurrentAccount.Username + "_" + item.ID_;
+                    Data.Ins.DB.CARTs.Add(new CART() { ID_ = tmpID, PRODUCT_ = item.ID_, USERNAME_ = CurrentAccount.Username, AMOUNT_ = 1 });
+                    Data.Ins.DB.SaveChanges();
+                    CustomMessageBox.Show("Đã thêm " + item.NAME_.ToString() + " vào giỏ hàng thành công", MessageBoxButton.OK, MessageBoxImage.Asterisk);
+                }
+                else
+                    CustomMessageBox.Show("Món ăn " + item.NAME_.ToString() + " đã có sẵn trong giỏ hàng", MessageBoxButton.OK, MessageBoxImage.Asterisk);
+            }
+            catch
+            {
+                CustomMessageBox.Show("Lỗi cơ sở dữ liệu", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
         }
         private void ItemClick(ListViewItem parameter)
         {
