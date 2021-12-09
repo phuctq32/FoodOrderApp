2a322f7fa3cf111bbeefce813c64442f7e212a81
phongbj <20520364@gm.uit.edu.vn>
2021-12-09 21:47:43 +0700

    add code logic for my other view model: load data

--- FoodOrderApp/FoodOrderApp/ViewModels/MyOrderViewModel.cs
+++ FoodOrderApp/FoodOrderApp/ViewModels/MyOrderViewModel.cs
@@ -1,5 +1,7 @@
 ﻿using FoodOrderApp.Models;
 using FoodOrderApp.Views;
+using FoodOrderApp.Views.UserControls;
+using FoodOrderApp.Views.UserControls.Admin;
 using MaterialDesignThemes.Wpf;
 using System;
 using System.Collections.Generic;
@@ -14,18 +16,40 @@ namespace FoodOrderApp.ViewModels
 {
     internal class MyOrderViewModel : BaseViewModel
     {
+        private List<RECEIPT> listReceipt;
+
+        public List<RECEIPT> ListReceipt
+        {
+            get => listReceipt;
+            set
+            {
+                listReceipt = value;
+                OnPropertyChanged("Status");
+                OnPropertyChanged("ListReceipt");
+            }
+        }
+        public int countItemInReceipt
+        {
+            get;
+            set;
+        }
+        public int Status;
         public ICommand RatingCommand { get; set; }
+        public ICommand LoadedCommand { get; set; }
+        public ICommand SelectionChangedCommand { get; set; }
         public ICommand OpenInvoiceCommand { get; set; }
         public ICommand PrintCommand { get; set; }
         //private List<RECEIPT> receipts;
 
-        // status = 1 là trạng thái chờ xác nhận
-        // status = 2 là trạng thái đang tiến hành
-        // status = 3 là trạng thái đã hoàn thành
-        // status = 4 là trạng thái đã huỷ
+        // status = 0 là trạng thái chờ xác nhận
+        // status = 1 là trạng thái đang tiến hành
+        // status = 2 là trạng thái đã hoàn thành
+        // status = 3 là trạng thái đã huỷ
         public MyOrderViewModel()
         {
             RatingCommand = new RelayCommand<RatingBar>(p => true, p => RatingChanged(p));
+            LoadedCommand = new RelayCommand<MyOrderUC>(p => p == null ? false : true, p => Load(p));
+            SelectionChangedCommand = new RelayCommand<MyOrderUC>((parameter) => { return true; }, (parameter) => SelectionChanged(parameter));
             //OpenInvoiceCommand = new RelayCommand<ListViewItem>(p => true, p => openInvoice(p));
             PrintCommand = new RelayCommand<InvoiceWindow>(paramater => true, paramater => print(paramater));
         }
@@ -50,19 +74,17 @@ namespace FoodOrderApp.ViewModels
                 paramater.controlBar.Visibility = Visibility.Visible;
             }
         }
-
-        //private void openInvoice(ListViewItem paramater)
-        //{
-        //    RECEIPT receipt = paramater.DataContext as RECEIPT;
-        //    if (CustomMessageBox.Show("Bạn có muốn in hoá đơn?", MessageBoxButton.YesNo, MessageBoxImage.Question) == MessageBoxResult.OK)
-        //    {
-        //        InvoiceWindow invoiceWindow = new InvoiceWindow();
-        //        invoiceWindow.listView.ItemsSource = receipts;
-        //        invoiceWindow.Show();
-        //    }
-
-        //    //receipt.STATUS_
-        //}
+        private void Load(MyOrderUC p)
+        {
+            ListReceipt = Data.Ins.DB.RECEIPTs.Where(receipt => receipt.STATUS_ == "0" && receipt.USER.USERNAME_ == CurrentAccount.Username).ToList();
+            Status = p.statusListViewUser.SelectedIndex;
+        }
+        private void SelectionChanged(MyOrderUC parameter)
+        {
+            ListReceipt.Clear();
+            Status = parameter.statusListViewUser.SelectedIndex;
+            ListReceipt = Data.Ins.DB.RECEIPTs.Where(receipt => receipt.STATUS_ == Status.ToString() && receipt.USER.USERNAME_ == CurrentAccount.Username).ToList();
+        }
 
         private void RatingChanged(RatingBar p)
         {
